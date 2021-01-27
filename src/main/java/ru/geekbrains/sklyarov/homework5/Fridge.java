620b4c644416c50080afb2c010a4199e3ae8f044
SklyarovEN <skevni@gmail.com>
2021-01-27 22:31:08 +0300

    Java Core 1, homework 5
    ООП + some class

---
+++ src/main/java/ru/geekbrains/sklyarov/homework5/Fridge.java
@@ -0,0 +1,33 @@
+package ru.geekbrains.sklyarov.homework5;
+
+public class Fridge {
+    private String model, manufacturer;
+    private float width, height, depth, volume, volumeOfFreezer;
+
+    public float getVolume() {
+        return volume;
+    }
+
+    public float getVolumeOfFreezer() {
+        return volumeOfFreezer;
+    }
+
+    public float setVolume(float volume){
+        return this.volume = volume;
+    }
+
+    public Fridge(String manufacturer, String model, float width, float height, float depth, float volume, float volumeOfFreezer) {
+        this.manufacturer = manufacturer;
+        this.model = model;
+        this.width = width;
+        this.height = height;
+        this.depth = depth;
+        this.volume = volume;
+        this.volumeOfFreezer = volumeOfFreezer;
+    }
+
+    public void printInfo(){
+        System.out.printf("Производитель: %s; Модель: %s; Ширина: %f; Высота: %f; Глубина: %f; Объем холодильной камеры: %f;" +
+                " Объем морозильной камеры: %f",manufacturer,model,width,height,depth,volume,volumeOfFreezer);
+    }
+}
